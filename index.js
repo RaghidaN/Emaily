34b433e32354826d3f2b2c2303eff9c3dd7bb03b
Raghida Sharif <raghidasharif@raghidas-mbp.mynetworksettings.com>
2023-10-18 14:38:42 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//backend has to use commonjs modules (NOT import ...)
+const express = require('express');
+//might have several express apps in one project, most projects will use single app object to set up configuration that listens for requests and routes them to handlers
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send({hi: 'there'});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
